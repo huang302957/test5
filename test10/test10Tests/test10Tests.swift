ead54f21ebc94ded7e5638de3e93c5a325789781
黄震宇 <huangzhenyu@huangzhenyudeMac.local>
2021-12-23 02:12:20 -0800

    444

---
+++ test10/test10Tests/test10Tests.swift
@@ -0,0 +1,36 @@
+//
+//  test10Tests.swift
+//  test10Tests
+//
+//  Created by 黄震宇 on 2021/12/23.
+//  Copyright © 2021年 黄震宇. All rights reserved.
+//
+
+import XCTest
+@testable import test10
+
+class test10Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
